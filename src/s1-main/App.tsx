311c50d0ec8e16bd3c7d05fde0288edfa9d6d7ad
GuzeevZhenya <shyst.evgeny@mail.ru>
2024-01-24 09:49:08 +0300

    done

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import s from "./App.module.css";
 import HW1 from "../s2-homeworks/hw01/HW1";
 import HW2 from "../s2-homeworks/hw02/HW2";
+import HW13 from "../s2-homeworks/hw13/HW13";
+
 
 function App() {
   return (
     <div className={s.App}>
       <HW1 />
       <HW2 />
+      <HW13 />
       {/*раскомментировать по ходу выполнения*/}
       {/*<HW2/>*/}
       {/*<HW3/>*/}
